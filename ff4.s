85c96b32530fce4494c02b3ab0d135f02078bbfc
Emmanuel Peralta <eperalta@webgeoservices.com>
2014-08-31 12:40:00 +0200

    a816 0.0.2 compatibility changes, code cleaning, adding menu code.

--- ff4.s
+++ ff4.s
@@ -1,10 +1,9 @@
 ; ----------------
 ; Final Fantasy IV the new hack.
 ; ----------------
-#include "src/libmz.i"
+.include 'src/libmz.i'
 
 dialog_bank_ptr_base = 0x218000
-
 
 
 ; déroutage pour ajouter le splash screen
@@ -17,36 +16,56 @@ dialog_bank_ptr_base = 0x218000
 	rts
 
 
-#include "src/vwf_patches.s"
+.include 'src/vwf_patches.s'
 
 
 ; include code and assets
 *=0x208000
-#include "src/libmz.s"
-#include "src/intro.s"
-#include "src/vwf.s"
-#include "src/dialog.s"
+.include 'src/libmz.s'
+.include 'src/intro.s'
+.include 'src/vwf.s'
+.include 'src/dialog.s'
+.include 'src/menus.s'
 
-*=0x21805D
+;*=0x21805D
 
-.table "text/ff4fr.tbl"
+;.table 'text/ff4fr.tbl'
+;*=0x2180FD
+;.pointer pt_intro_test_de_la_mort
+;pt_intro_test_de_la_mort:
+;.db $04,$00
+;.db $04,$01
+;.db $04,$02
+;.db $04,$03
+;.db $04,$04
 
-.dw pt_intro_test_de_la_mort & 0xFFFF
-.db pt_intro_test_de_la_mort >> 16
-pt_intro_test_de_la_mort:
-.text "ABCDEFGHIJ"
+;.db $00
 
-.db 0x00
+*=0x218000
+.incbin 'assets/bank1.ptr'
+;*=0x218600
+;.incbin 'assets/bank2.ptr'
+;.incbin 'assets/bank3.ptr'
+
+*=0x228000
+.incbin 'assets/bank1.dat'
+
+;*=0x238000
+;.incbin 'assets/bank2.dat'
+
+;*=0x248000
+;.incbin 'assets/bank3.dat'
 
 *=0x258000
-.incbin "assets/font.dat"
-.incbin "assets/font_length_table.dat"
+.incbin 'assets/font.dat'
+.incbin 'assets/font_length_table.dat'
+
 
 *=0x278000
 ; Splash screen assets
-.incbin "assets/intro.map"
-.incbin "assets/intro.col"
-.incbin "assets/intro.set"
+.incbin 'assets/intro.map'
+.incbin 'assets/intro.col'
+.incbin 'assets/intro.set'
 
 
 
