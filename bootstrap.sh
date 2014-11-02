87178b5bb66e0fa27640089a0bf7c823abf1e897
Emmanuel Peralta <eperalta@webgeoservices.com>
2014-11-02 12:50:07 +0100

    Adds travis configuration, gitignore and a bootstrap script

---
+++ bootstrap.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+set -e
+
+# clone the repository
+git clone https://github.com/manz/ff4.git ff4
+cd ff4
+
+# create the python 3 virtualenv
+virtualenv VE_ff4 -p `python3.4-config --exec-prefix`/bin/python-3.4
+
+# install dependencies
+source VE_ff4/bin/activate
+pip install -r requirements.txt
+
+# build the patch ?
+./build.py
