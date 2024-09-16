309253e4bf0a00c3be474679d18b2194a3c61882
Emmanuel Peralta <manz@ringum.net>
2024-09-16 23:23:19 +0200

    Updates menu.

--- src/ingame/options.s
+++ src/ingame/options.s
@@ -1,25 +1,27 @@
 
 ;RGB -> RVB :o)
 *=0x01D1BB
-	lda.b #0x57
+    lda.b #0x57
 
 ; déplacement du curseur principal des options
 *=0x01D247
-	lda.b #0x00
+    lda.b #0x00
 
 ; Cursor offset in controls menu (x)
 *=0x01D4E6
-	lda.b #0x03
+    lda.b #0x03
 
-;y
+; cursor y
 *=0x01D4E2
-	adc.b #0x4C
+    adc.b #0x4C
 
 *=0x01D1B0
-	load_system_menu_text_pointer(options.title)
+    load_system_menu_text_pointer(options.title)
 
 *=0x01D1A4
-	load_system_menu_text_pointer(options.config)
+    load_system_menu_text_pointer(options.config)
+    ;jsr.w draw_window_and_vwf_message
+
 
 ; move controls title window
 *=0x01E204
@@ -33,4 +35,4 @@
 
 ; controles
 *=0x01D48D
-	load_system_menu_text_pointer(options.controls)
+    load_system_menu_text_pointer(options.controls)
--- src/menus/tools_shop_text.s
+++ src/menus/tools_shop_text.s
@@ -1,30 +1,33 @@
+.include 'src/ingame/macros.i'
+
 .scope shops {
 gils:
-	.dw 0x0176 ; - 4
-	{
-	.table 'text/vwf_precomp.tbl'
-	.text 'Gils'
-	}
-	.db 0
+    ;.dw 0x0176 ; - 4
+    move_to(27, 6)
+;    {
+;    .table 'text/vwf_precomp.tbl'
+    .text 'Gils'
+;    }
+    .db 0
 
 welcome_and_actions:
-	.dw 0x0054 - 2
-	.text 'Puis-je vous aider ?'
-	;.text 'いらっしゃい! どんなごようけんで?'
-	.db 1
-	.dw 0x0148 - 4
-	.text 'Achat Vente Sortir'
-	.db 0
-	;.text 'かう   うる   でる'
+    .dw 0x0054 - 2
+    .text 'Puis-je vous aider ?'
+    ;.text 'いらっしゃい! どんなごようけんで?'
+    .db 1
+    .dw 0x0148 - 4
+    .text 'Achat Vente Sortir'
+    .db 0
+    ;.text 'かう   うる   でる'
 
 quantity:
-	.dw 0x0052
-	.text 'Que désirez vous ?'
-	.db 1
-	.dw 0x0144
-	.text 'Quantité'
-	.db 1
-	.dw 0x0146 + 8 * 2
-	.text '1'
-	.db 0
+    .dw 0x0052
+    .text 'Que désirez vous ?'
+    .db 1
+    .dw 0x0144
+    .text 'Quantité'
+    .db 1
+    .dw 0x0146 + 14 * 2
+    .text '1'
+    .db 0
 }
