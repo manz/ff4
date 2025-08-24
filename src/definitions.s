b1f86c1cb6de56cdacaf44aefffb3dfb20166ea9
Emmanuel Peralta <eperalta@woosmap.com>
2025-08-24 21:53:31 +0200

    Write down somewhere wat's the layout of the sram.

--- src/definitions.s
+++ src/definitions.s
@@ -1,3 +1,10 @@
+; S-RAM layout
+; 0x700000 - 0x701fff: Save data
+; 0x702000 - 0x702fff: Tile buffer for dialog text renderer
+; 0x703000 - 0x7032FF: Tile buffer used in battle text renderer and menu text renderer
+; 0x705000 - 0x706300: Vram saved before entering menus.
+; 0x707000 - 0x709cff: Battle magic buffers
+; 0x710000 - 0x710100: Vars buffer
 .scope field_vwf {
     tile_buffer = 0x702000
 }
